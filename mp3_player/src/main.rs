111d496d3fc0ee2cda9055c9d7448ed7dd9d672d
Hector Valdes <hdvvaldes@gmail.com>
2026-04-16 18:53:42 -0600

    refactor(struchture) folders

--- mp3_player/src/main.rs
+++ mp3_player/src/main.rs
@@ -1,3 +1,11 @@
+
+mod mp3_player;
+
+use mp3_player::App;
+
 fn main() {
-    println!("Hello, world!");
+    let mp3_player = App::new("TUI");
+    mp3_player.start();
+    let e_code:u8 = mp3_player.run();
+    mp3_player.close(e_code);
 }
