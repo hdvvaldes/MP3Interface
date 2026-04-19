47c9070d42ab08c85d8c4239fd845f10f4c6637a
Hector Valdes <hdvvaldes@gmail.com>
2026-04-18 18:07:07 -0600

    dividing the song out of miner song

--- domain/src/song.rs
+++ domain/src/song.rs
@@ -1,18 +1,8 @@
-use crate::person::Person;
-use crate::group::Group;
-use crate::album::Album;
-
-#[derive(Debug, Clone, PartialEq)]
-pub enum Performer {
-    Single(Person), 
-    Multiple(Group),
-}
-
 #[derive(Debug, Clone, PartialEq)]
 pub struct Song {
     pub title: String,
-    pub performer: Performer,
-    pub album: Album,
+    pub performer: String,
+    pub album: String,
     pub track: Option<String>,
     pub year: Option<String>,
     pub genre: Option<String>,
