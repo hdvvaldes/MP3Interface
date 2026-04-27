75e0f72dc1d728c472715fa9c4f2978853036c9e
Hector Valdes <hdvvaldes@gmail.com>
2026-04-26 23:42:45 -0600

    feature: Compiles a simple variable

--- compiler/prop2sql/src/Parser.hs
+++ compiler/prop2sql/src/Parser.hs
@@ -6,19 +6,19 @@ module Parser
   parseInput,
 ) where 
 
-import Text.Megaparsec (Parsec, MonadParsec (takeWhile1P, takeWhileP), empty)
+import Text.Megaparsec (Parsec, MonadParsec (takeWhile1P, takeWhileP), empty, between, (<|>))
 import qualified Text.Megaparsec.Char.Lexer as L
 import Data.Char (isAlpha, isAlphaNum)
 import qualified Data.Text as T
 import Domain (SQLTable, Prop(DISY, VAR))
 import Data.Void (Void)
-import Text.Megaparsec.Char (space1)
+import Text.Megaparsec.Char (space1, string)
 
 type Parser = Parsec Void T.Text
 
 parseInput :: Parser (SQLTable, Prop)
 parseInput = do
-  table <- parseTable
+  table <- parseID
   _ <- symbol ":"
   prop <- parseProp
   return (table,prop)
@@ -29,8 +29,8 @@ sc = L.space space1 empty empty
 symbol :: T.Text -> Parser T.Text
 symbol = L.symbol sc
 
-parseTable :: Parser SQLTable
-parseTable = do
+parseID :: Parser T.Text
+parseID = do
     firstPart <- takeWhile1P (Just "letter or underscore") 
       (\c -> isAlpha c || c == '_')
     restPart  <- takeWhileP (Just "alphanumeric or underscore") 
@@ -38,17 +38,17 @@ parseTable = do
     return (firstPart <> restPart)
 
 parseProp :: Parser Prop
-parseProp = do 
-    left <- parseTerm
-    right <- parseTerm
-    return $ DISY left right
+parseProp = parens parseProp <|> parseVar
+
+parens :: Parser a -> Parser a
+parens = between (string "(") (string ")")
+
+parseVar :: Parser Prop
+parseVar = VAR <$> parseID
 
 -- | Return a constructor for the parsed Prop
 identifyOp :: Parser Prop
 identifyOp = do 
     some <- foldMap symbol ["AND", "OR"]
     return $ VAR "some"
-    
-parseTerm :: Parser Prop
-parseTerm = undefined
 
