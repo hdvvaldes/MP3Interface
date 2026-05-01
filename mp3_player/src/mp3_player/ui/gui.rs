69de000a608f3a941d48da862875733084b9b984
Hector Valdes <hdvvaldes@gmail.com>
2026-04-30 21:12:50 -0600

    user_actions: enum to handle events in the view

--- mp3_player/src/mp3_player/ui/gui.rs
+++ mp3_player/src/mp3_player/ui/gui.rs
@@ -1,4 +1,4 @@
-use crate::mp3_player::ui::PlayerView;
+use crate::mp3_player::ui::view_api::{PlayerView, PlayerAction};
 use crate::mp3_player::controller::AppState;
 
 pub struct GUIView;
@@ -9,23 +9,29 @@ impl GUIView {
     }
 }
 
-
 impl PlayerView for GUIView {
+    fn setup(&mut self) -> Result<(), String> {
+        Ok(())
+    }
 
+    fn render(&mut self, _state: &AppState) -> Result<(), String> {
+        // GUI rendering logic (e.g. using iced or egui)
+        Ok(())
+    }
 
-
-    fn render(&mut self, state: &AppState) {
-        
+    fn handle_events(&mut self) -> PlayerAction {
+        PlayerAction::None
     }
 
     fn display_error(&self, reason: &str) {
-        
+        eprintln!("GUI Error: {}", reason);
     }
 
     fn display_message(&self, msg: &str) {
-        
+        println!("GUI Info: {}", msg);
     }
 
-
-
+    fn teardown(&mut self) -> Result<(), String> {
+        Ok(())
+    }
 }
--- mp3_player/src/mp3_player/ui/view_api.rs
+++ mp3_player/src/mp3_player/ui/view_api.rs
@@ -1,14 +1,28 @@
+use domain::Song;
+
 use crate::mp3_player::controller::AppState;
 
+pub enum PlayerAction {
+    Play(Song),
+    Pause,
+    Resume,
+    Next,
+    Previous,
+    Search(String),
+    Quit,
+    None,
+}
+
 pub trait PlayerView {
-    
     fn setup(&mut self) -> Result<(), String>;
 
-    fn render(&mut self, state: &AppState);
+    fn render(&mut self, state: &AppState) -> Result<(), String>;
+
+    fn handle_events(&mut self) -> PlayerAction;
 
     fn display_message(&self, msg: &str);
 
     fn display_error(&self, reason: &str);
-    
-    fn teardown(&mut self) -> Result<(),String>;
+
+    fn teardown(&mut self) -> Result<(), String>;
 }
